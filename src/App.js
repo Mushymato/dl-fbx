0f9777845e8278d70d3a4cbeedc9a6b85c35815c
mushymato <wildshinobu@gmail.com>
2019-11-26 02:53:16 -0500

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Model from './Model';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Redirect to="/dl-fbx/c110349_01" />
+
         <Switch>
-          <Route path="/:id" children={<Model />} />
+          <Route path="/dl-fbx/:id" children={<Model />} />
         </Switch>
       </BrowserRouter>)
   }
